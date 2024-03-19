bcccb53023927cd8dcd5b4ec946fed37a9aa4477
Nils <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-19 17:34:39 +0100

    docs: add comments on the move schema

--- src/main/resources/db/migration/V1__cache_tables.sql
+++ src/main/resources/db/migration/V1__cache_tables.sql
@@ -1,9 +1,9 @@
 create type direction as enum ('NORTH', 'EAST', 'SOUTH', 'WEST');
 create table move
 (
-    id serial primary key,
-    direction direction,
-    steps int,
-    x int,
-    y int
+    id serial primary key, -- todo: maybe a timestamp would be better
+    direction direction, -- the direction of the move
+    steps int, -- the number of steps that were performed
+    x int, -- the x coordinate of the position after the move
+    y int -- the y coordinate of the position after the move
 );
