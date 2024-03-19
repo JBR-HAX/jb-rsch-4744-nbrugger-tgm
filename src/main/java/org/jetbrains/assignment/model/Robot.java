a78f9d8051752fc9c60af85c70dfd5167a0d2281
Nils <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-19 17:35:01 +0100

    fix: robot moving only one step at a time

--- src/main/java/org/jetbrains/assignment/model/Robot.java
+++ src/main/java/org/jetbrains/assignment/model/Robot.java
@@ -8,11 +8,11 @@ public class Robot {
     }
 
     public void move(Move move) {
-        switch (move.dir()) {
-            case EAST -> position = new Position(position.x() + 1, position.y());
-            case WEST -> position = new Position(position.x() - 1, position.y());
-            case NORTH -> position = new Position(position.x(), position.y() + 1);
-            case SOUTH -> position = new Position(position.x(), position.y() - 1);
+        switch (move.direction()) {
+            case EAST -> position = new Position(position.x() + move.steps(), position.y());
+            case WEST -> position = new Position(position.x() - move.steps(), position.y());
+            case NORTH -> position = new Position(position.x(), position.y() + move.steps());
+            case SOUTH -> position = new Position(position.x(), position.y() - move.steps());
         }
     }
 
