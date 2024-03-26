5752f1ea524e3beefd40c4f92c812cf81c522fff
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2024-03-26 17:36:50 +0100

    chores: add database and migration dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter")
     //add dependencies as needed
